e997f3782d785de30ed0b58ac920c0acac9c74a2
Igor Pimenta <igorjpimenta@gmail.com>
2024-06-14 08:22:07 -0300

    Understanding the structure

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Pimenta'
+    })
+
+
+def about(request):
+    return HttpResponse('About')
+
+
+def contact(request):
+    return HttpResponse('Contact')
